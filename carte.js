3dab4e61679f17513721936a48177538a42f524d
robertsergo <robertsergodimanche@gmail.com>
2022-10-13 11:16:56 -0400

    bouillon du projet carte

---
+++ carte.js
@@ -0,0 +1,65 @@
+
+function affichercarte(){
+    let list_pique = []
+    let list_carreau = []
+    let list_trefle = []
+    let list_coeur = []
+    let pique = document.querySelector(".pique").innerHTML
+    let carreau = document.querySelector(".carreau").textContent
+    let trefle = document.querySelector(".trefle").textContent
+    let coeur = document.querySelector(".coeur").innerHTML
+
+    
+
+    for(let i = 2; i < 14; i++){
+        list_pique[i] = pique
+        list_carreau[i] = carreau
+        list_trefle[i] = trefle
+        list_coeur[i] = coeur
+    }
+       
+    document.write("<table style='text-align:center'>")
+
+    
+    for(let i = 2; i < list_pique.length; i++){  
+        document.write(" <tr>") 
+            
+
+        
+       document.write(" <td style='font-size: 10rem; background-color: powderblue;'>")
+        document.write(i+""+list_pique[i]+"    "); 
+        document.write(" </td>")
+    
+   
+        
+
+        document.write(" <td style='color:red; font-size: 10rem; background-color: powderblue;'>")
+        document.write(i+""+list_carreau[i]+"    "); 
+        document.write(" </td>") 
+
+        document.write(" <td style='font-size: 10rem; background-color: powderblue;'>")
+        document.write(i+""+list_trefle[i]+"    ");
+        document.write(" </td>")
+
+        document.write(" <td style='font-size: 10rem; color:red; background-color: powderblue; '>")
+        document.write(i+""+list_coeur[i]+"    "); 
+        document.write(" </td>")
+        //document.write("<br/>"); 
+
+        document.write(" </tr>")          
+        
+    }
+    
+    document.write(" </table>")
+}
+
+
+
+
+
+
+
+
+function myFunction() {
+    document.getElementById("afficher").innerHTML = "Hello World";
+}
