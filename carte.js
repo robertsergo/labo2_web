60960058b2f95da5fff8d68f97d56ae0bc58906a
robertsergo <111302645+robertsergo@users.noreply.github.com>
2022-10-14 14:11:19 -0400

    affichage ameliore

--- carte.js
+++ carte.js
@@ -5,6 +5,7 @@ let list_carreau = []
 let list_trefle = []
 let list_coeur = []
 let list_carte = []
+let paquet = []
 let paquet1 = []
 let paquet2 = []
 let pique = document.querySelector(".pique").innerHTML
@@ -57,16 +58,29 @@ function affichercarte(){
     }
     */
 
+   // document.querySelector("#ma_table").innerHTML = "<table>"
+   // document.querySelector("#ma_ligne").innerHTML = "<tr>"
+
        
     
-    for(let i = 1; i < 14; i++){        
+    for(let i = 1; i < list_pique.length; i++){ 
 
         document.querySelector("#afficher_pique").innerHTML += list_pique[i];
         document.querySelector("#afficher_carreau").innerHTML += list_carreau[i];
         document.querySelector("#afficher_trefle").innerHTML += list_trefle[i]; 
-        document.querySelector("#afficher_coeur").innerHTML += list_coeur[i];     
-       
+        document.querySelector("#afficher_coeur").innerHTML += list_coeur[i];       
     }   
+
+    for(let i = 1; i < list_pique.length; i++){ 
+
+        list_pique[i]= "";
+        list_carreau[i]= "";
+        list_trefle[i]= "";
+        list_coeur[i]= "";      
+    }   
+    //("</tr>")
+
+  // ("</table>")
 }
 
 
@@ -74,6 +88,25 @@ function affichercarte(){
 
 
 function brasser() {
-    document.getElementById("afficher_brasser").innerHTML = "Hello World";
-    
+    //for(let i = 1; i < list_carreau.length; i++){  
+       // document.getElementById("afficher_brasser").innerHTML = list_carte.length; 
+        paquet1 = list_carreau.concat(list_pique)
+        paquet2 = list_coeur.concat(list_trefle)
+        paquet = paquet1.concat(paquet2)
+
+    //}
+
+    for(let i = 1; i < 52; i++){
+        if(i ==13 || i == 26 || i == 39){
+            document.createElement("</br>")        
+
+        }      
+
+        
+        document.querySelector("#afficher_brasser").innerHTML += paquet[i];
+       // document.querySelector("#afficher_trefle").innerHTML += ""; 
+       // document.querySelector("#afficher_coeur").innerHTML += "";     
+       
+    }     
+
 }
